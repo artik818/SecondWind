5b0dd363aada7b6db64fef38c1e155c32170b7ab
Momus <admin@Admins-Mac.local>
2014-04-29 02:43:10 +0400

    mml

--- SecondWind/Classes/Helpers/Providers/SWMediaLibraryProvider.m
+++ SecondWind/Classes/Helpers/Providers/SWMediaLibraryProvider.m
@@ -7,6 +7,7 @@
 //
 
 #import "SWMediaLibraryProvider.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 static SWMediaLibraryProvider *globalMediaManager = nil;
 
@@ -26,9 +27,56 @@ static SWMediaLibraryProvider *globalMediaManager = nil;
 {
     self = [super init];
     if (self) {
+        
     }
     return self;
 }
 
+
+- (NSArray *)getAllMedia {
+    MPMediaQuery *everything = [[MPMediaQuery alloc] init];
+    
+    NSLog(@"Logging items from a generic query...");
+    NSArray *itemsFromGenericQuery = [everything items];
+    for (MPMediaItem *song in itemsFromGenericQuery) {
+        NSString *songTitle = [song valueForProperty: MPMediaItemPropertyTitle];
+        NSLog (@"%@", songTitle);
+    }
+    
+    return itemsFromGenericQuery;
+}
+
+- (NSArray *)getAllMediaWithArtist:(NSString *)artistName {
+    MPMediaPropertyPredicate *artistNamePredicate = [MPMediaPropertyPredicate predicateWithValue:artistName
+                                                                                     forProperty:MPMediaItemPropertyArtist];
+    
+    MPMediaQuery *myArtistQuery = [[MPMediaQuery alloc] init];
+    [myArtistQuery addFilterPredicate:artistNamePredicate];
+    
+    NSArray *itemsFromArtistQuery = [myArtistQuery items];
+    
+    return itemsFromArtistQuery;
+}
+
+- (void)getComplex {
+    
+    MPMediaPropertyPredicate *artistNamePredicate =
+    [MPMediaPropertyPredicate predicateWithValue: @"Sad the Joker"
+                                     forProperty: MPMediaItemPropertyArtist];
+    
+    MPMediaPropertyPredicate *albumNamePredicate =
+    [MPMediaPropertyPredicate predicateWithValue: @"Stair Tumbling"
+                                     forProperty: MPMediaItemPropertyAlbumTitle];
+    
+    NSSet *predicates = [NSSet setWithObjects:artistNamePredicate, albumNamePredicate, nil];
+    
+    MPMediaQuery *specificQuery = [[MPMediaQuery alloc] initWithFilterPredicates:predicates];
+    NSArray *itemsFromArtistQuery = [specificQuery items];
+}
+
+- (void)getArtworkForMediaitem:(MPMediaItem *)mediaItem withSie:(CGSize)imageSize {
+    MPMediaItemArtwork *artwork = [mediaItem valueForProperty:MPMediaItemPropertyArtwork];
+    UIImage *artworkImage = [artwork imageWithSize:imageSize];
+}
 
 @end
